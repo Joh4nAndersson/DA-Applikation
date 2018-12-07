826ab6279098ab5a0f08ca3686b4e0c8f792187a
evan_ <evan_@192.168.10.125>
2018-12-07 22:51:52 +0100

    moved database calls to integration

---
+++ functions/integration/dbhandler.js
@@ -0,0 +1,30 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../misc/serviceAccountKey.json");
+const cts = require('../misc/constants');
+const ERROR_MSG = cts.error_msg;
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://newagent-44155.firebaseio.com"
+});
+
+function get_calID_by_name(fname, lname) {
+    var db = admin.database();
+    var key = 'calendars/';
+    var search = db.ref(key)
+            .orderByChild('name')
+            .equalTo(fname.toLowerCase() + " " + lname.toLowerCase());
+
+    return new Promise((resolve, reject) => {
+        search.once('value', (snapshot) => {
+            resolve(snapshot.val());
+        }, (error) => {
+            console.log("Database error: ", error);
+
+            reject(ERROR_MSG);
+        });
+    });
+
+}
+
+module.exports.getCalenderID = get_calID_by_name;
--- functions/provider/request_handler.js
+++ functions/provider/request_handler.js
@@ -1,22 +1,13 @@
-const admin = require("firebase-admin");
+
 const events = require("./events");
 const findEvent = events.findEvent;
 const findEvents = events.findEvents;
+const getCalenderID = require("../integration/dbhandler").getCalenderID;
 
 const cts = require('../misc/constants');
 const ERROR_MSG = cts.error_msg;
 const DEFAULT_RESPONSE = cts.default_response;
 const empty = cts.is_empty;
-
-const serviceAccount = require("../misc/serviceAccountKey.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://newagent-44155.firebaseio.com"
-});
-
-var db = admin.database();
-var key = 'calendars/';
 
 function simple_request(conv) {
     var params = conv.parameters;
@@ -31,26 +22,25 @@ function simple_request(conv) {
     var reqType = params['request-entity'];
 
     return new Promise(function (resolve, reject) {
-        var search = db.ref(key)
-                .orderByChild('name')
-                .equalTo(fname.toLowerCase() + " " + lname.toLowerCase());
+        return getCalenderID(fname, lname)
+                .then((value) => {
+                    if (value === null || value === 'null') {
+                        resolve("Jag kan tyvärr inte hitta någon information på personen.");
+                        return;
+                    }
+                    var calendarID = value[0].calendarID;
+                    findEvents(calendarID, new Date(), null, fname, reqType)
+                            .then((resp) => {
+                                resolve(resp);
+                            })
+                            .catch((error) => {
+                                reject(error);
+                            });
+                })
+                .catch((error) => {
+                    reject(error);
+                });
 
-        search.once('value', (snapshot) => {
-            var value = snapshot.val();
-
-            if (value === null || value === 'null') {
-                resolve("Jag kan tyvärr inte hitta någon information på personen.");
-                return;
-            }
-            var calendarID = value[0].calendarID;
-            findEvents(calendarID, new Date(), null, fname, reqType)
-                    .then((resp) => {
-                        resolve(resp);
-                    })
-                    .catch((error) => {
-                        reject(error);
-                    });
-        });
     });
 }
 function advanced_request(conv) {
@@ -66,59 +56,50 @@ function advanced_request(conv) {
     var datePeriod = params['date-period'];
 
     return new Promise(function (resolve, reject) {
-        var search = db.ref(key)
-                .orderByChild('name')
-                .equalTo(fname.toLowerCase() + " " + lname.toLowerCase());
+        return getCalenderID(fname, lname)
+                .then((value) => {
+                    if (value === null || value === 'null') {
+                        resolve("Jag kan tyvärr inte hitta någon information på personen.");
+                        return;
+                    }
+                    var calendarID = value[0].calendarID;
+                    var startdate;
+                    var enddate;
+                    if (empty(datetime) && empty(date) && empty(datePeriod)) {
+                        startdate = new Date();
+                        enddate = null;
+                    } else if (!empty(datetime)) {
+                        if (datetime.hasOwnProperty('startDateTime')) {
+                            startdate = datetime.startDateTime;
+                            enddate = datetime.endDateTime;
+                        } else {
+                            startdate = datetime;
+                            enddate = null;
+                        }
+                    } else if (!empty(date)) {
+                        datetime = new Date(date);
+                        startdate = datetime.setHours(0, 0, 0, 0);
+                        enddate = datetime.setHours(23, 59, 59, 999);
 
-        search.once('value', (snapshot) => {
-            var value = snapshot.val();
-            if (value === null || value === 'null') {
-                resolve("Jag kan tyvärr inte hitta någon information på personen.");
-                return;
-            }
-            var calendarID = value[0].calendarID;
-            var startdate;
-            var enddate;
-            if (empty(datetime) && empty(date) && empty(datePeriod)) {
-                startdate = new Date();
-                enddate = null;
-            } 
-            else if (!empty(datetime)) {
-                if (datetime.hasOwnProperty('startDateTime')) {
-                    startdate = datetime.startDateTime;
-                    enddate = datetime.endDateTime;
-                } else {
-                    startdate = new Date();
-                    enddate = null;
-                }
-            } 
-            else if (!empty(date)) {
-                datetime = new Date(date);
-                startdate = datetime.setHours(0, 0, 0, 0);
-                enddate = datetime.setHours(23, 59, 59, 999);
+                    } else if (!empty(datePeriod)) {
+                        startdate = (new Date(datePeriod.startDate)).setHours(0, 0, 0, 0);
+                        enddate = (new Date(datePeriod.endDate)).setHours(23, 59, 59, 999);
 
-            } 
-            else if (!empty(datePeriod)) {
-                startdate = (new Date(datePeriod.startDate)).setHours(0, 0, 0, 0);
-                enddate = (new Date(datePeriod.endDate)).setHours(23, 59, 59, 999);
+                    } else {
+                        resolve(DEFAULT_RESPONSE);
+                        return;
+                    }
+                    findEvents(calendarID, startdate, enddate, fname, reqType)
+                            .then((resp) => {
+                                resolve(resp);
+                            })
+                            .catch((error) => {
+                                reject(error);
+                            });
 
-            } 
-            else {
-                resolve(DEFAULT_RESPONSE);
-                return;
-            }
-            findEvents(calendarID, startdate, enddate, fname, reqType)
-                    .then((resp) => {
-                        resolve(resp);
-                    })
-                    .catch((error) => {
-                        reject(error);
-                    });
-
-        }, (error) => {
-            console.log("Database error: ", error);
-            reject(ERROR_MSG);
-        });
+                }, (error) => {
+                    reject(error);
+                });
     });
 }
 
